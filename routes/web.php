26d25e3f850c4011c079f2de76fc68d4575234a6
Henry Garcia <hjgarcia1@gmail.com>
2019-01-14 20:54:14 -0500

    removed all conceptua math forms and tests

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,5 @@ Route::post('/access-request', 'AccessRequestController@store')->name('access_re
 Route::get('/digital-setup-request', 'DigitalSetupController@create')->name('digital_setup_request.create');
 Route::post('/digital-setup-request', 'DigitalSetupController@store')->name('digital_setup_request.store');
 
-Route::get('/conceptua-math/request-a-demo', 'RequestDemoController@create')->name('conceptua.request_demo.create');
-Route::post('/conceptua-math/request-a-demo', 'RequestDemoController@store')->name('conceptua.request_demo.store');
-
-Route::get('/conceptua-math/request-a-quote', 'RequestQuoteController@create')->name('conceptua.request_quote.create');
-Route::post('/conceptua-math/request-a-quote', 'RequestQuoteController@store')->name('conceptua.request_quote.store');
-
-Route::get('/conceptua-math/request-a-case-study', 'RequestCaseController@create')->name('conceptua.request_case.create');
-Route::post('/conceptua-math/request-a-case-study', 'RequestCaseController@store')->name('conceptua.request_case.store');
-
 Route::get('/return-request', 'ReturnRequestController@create')->name('return_request.create');
 Route::post('/return-request', 'ReturnRequestController@store')->name('return_request.store');
