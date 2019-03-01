e95315adfc3d55626c8e02f1cac39cde6755bc9d
Henry Garcia <hjgarcia1@gmail.com>
2019-02-28 21:57:52 -0500

    fixed webpack file

--- tests/Feature/CreateNextGenPetSupportForm.php
+++ tests/Feature/CreateNextGenPetSupportForm.php
@@ -15,8 +15,9 @@ class CreateNextGenPetSupportForm extends TestCase
      */
     public function we_can_see_the_access_request_form_page()
     {
-        $this->get(route('nextgen_pet.create'))
-            ->assertStatus(200)
+        $response = $this->get(route('nextgen_pet.create'));
+
+        $response->assertStatus(200)
             ->assertViewIs('nextgen_pet_request.create');
     }
 }
