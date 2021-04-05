733a48307d533e3724d6020fdb81679c72efe7d2
Henry Garcia <hgarcia@activatelearning.com>
2021-04-05 12:23:40 -0400

    new validation tests added

--- tests/Feature/App/Http/Requests/CalculatorRequestTest.php
+++ tests/Feature/App/Http/Requests/CalculatorRequestTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\App\Http\Requests;
 
 use App\Http\Requests\CalculatorRequest;
+use Faker\Factory;
+use Symfony\Component\HttpFoundation\Request;
 use Tests\TestCase;
 
 /**
@@ -11,20 +13,118 @@ use Tests\TestCase;
  */
 class CalculatorRequestTest extends TestCase
 {
-    public function test_the_form_request_returns_the_correct_rules()
+    /**
+     * @dataProvider validData
+     */
+    public function test_the_form_request_is_valid(array $data)
     {
-        $rules = [
-            'first_name' => 'required',
-            'last_name'  => 'required',
-            'email'      => 'required|email',
-            'role'       => 'required',
-            'school'     => 'required',
-            'city'       => 'required',
-            'state'      => 'required',
-        ];
-
         $request = new CalculatorRequest();
 
-        $this->assertEquals($rules, $request->rules());
+        $validator = \Validator::make($data, $request->rules());
+
+        $this->assertTrue($validator->passes());
     }
+
+    /**
+     * @dataProvider invalidData
+     */
+    public function test_the_form_request_is_invalid(array $data)
+    {
+        $request = new CalculatorRequest();
+
+        $validator = \Validator::make($data, $request->rules());
+
+        $this->assertFalse($validator->passes());
+    }
+
+    public function validData(): array
+    {
+        $faker = Factory::create(Factory::DEFAULT_LOCALE);
+
+        return [
+            [[
+                 'first_name'       => $faker->firstName,
+                 'last_name'        => $faker->lastName,
+                 'email'            => $faker->safeEmail,
+                 'role'             => $faker->title,
+                 'school'           => $faker->company,
+                 'city'             => $faker->city,
+                 'state'            => $faker->state,
+             ]]
+        ];
+    }
+
+    public function invalidData(): array
+    {
+        $faker = Factory::create(Factory::DEFAULT_LOCALE);
+
+        return [
+            [[//first name is required
+                 'last_name'        => $faker->lastName,
+                 'email'            => $faker->safeEmail,
+                 'role'             => $faker->title,
+                 'school'           => $faker->company,
+                 'city'             => $faker->city,
+                 'state'            => $faker->state,
+             ]],
+            [[//last name is required
+              'first_name'       => $faker->firstName,
+              'email'            => $faker->safeEmail,
+              'role'             => $faker->title,
+              'school'           => $faker->company,
+              'city'             => $faker->city,
+              'state'            => $faker->state,
+             ]],
+            [[//email is required
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'role'             => $faker->title,
+              'school'           => $faker->company,
+              'city'             => $faker->city,
+              'state'            => $faker->state,
+             ]],
+            [[//is valid
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'email'            => 'email-invalid',
+              'role'             => $faker->title,
+              'school'           => $faker->company,
+              'city'             => $faker->city,
+              'state'            => $faker->state,
+             ]],
+            [[//role is required
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'email'            => $faker->safeEmail,
+              'school'           => $faker->company,
+              'city'             => $faker->city,
+              'state'            => $faker->state,
+             ]],
+            [[//school is required
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'email'            => $faker->safeEmail,
+              'role'             => $faker->title,
+              'city'             => $faker->city,
+              'state'            => $faker->state,
+             ]],
+            [[//city is required
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'email'            => $faker->safeEmail,
+              'role'             => $faker->title,
+              'school'           => $faker->company,
+              'state'            => $faker->state,
+             ]],
+            [[//state is required
+              'first_name'       => $faker->firstName,
+              'last_name'        => $faker->lastName,
+              'email'            => $faker->safeEmail,
+              'role'             => $faker->title,
+              'school'           => $faker->company,
+              'city'             => $faker->city,
+             ]],
+        ];
+    }
+
 }
