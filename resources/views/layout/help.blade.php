f9c2df52f2957c0ccc141174bd84a334571f86a3
Henry Garcia <hgarcia@activatelearning.com>
2021-04-07 13:29:25 -0400

    constrained header

--- resources/views/layout/help.blade.php
+++ resources/views/layout/help.blade.php
@@ -12,34 +12,35 @@
 
 <body>
     <div class="h-full grid-help">
-        <header class="page-header mb-10">
-            <div class="overlay relative h-full py-2 px-3 flex justify-between items-center md:py-10 md:px-5">
-                <div class="md:hidden text-white">
-                    <a href="">
+        <header class="page-header mb-10 relative">
+            <div class="overlay h-full">
+                <div class="h-full py-2 px-3 flex justify-between items-center md:py-10 md:px-5 md:w-4/6 mx-auto">
+                    <div class="md:hidden text-white">
+                        <a href="">
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="https://help.activatelearning.com/s/">
+                            <img src="{{ asset('img/logo-w.png') }}" class="w-48 md:w-64" alt="Activate Learning">
+                        </a>
+                    </div>
+                    <nav class="absolute hidden  flex flex-col bg-white w-full left-0 top-12 md:flex-row md:relative md:top-0 z-10 md:gap-20 md:ml-auto md:bg-transparent md:w-auto md:block">
+
+                        <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent" href="https://help.activatelearning.com/s/">Home</a>
+
+                        <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent" href="https://help.activatelearning.com/s/system-status">System Status</a>
+
+                        <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent" href="https://help.activatelearning.com/s/contactsupport">Contact Support</a>
+                    </nav>
+                    <div class="text-white md:ml-16">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                         </svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://help.activatelearning.com/s/">
-                        <img src="{{ asset('img/logo-w.png') }}" class="w-48 md:w-64" alt="Activate Learning">
-                    </a>
-                </div>
-                <nav class="absolute hidden  flex flex-col bg-white w-full left-0 top-12 md:flex-row md:relative md:top-0 z-10 md:gap-20 md:ml-auto md:bg-transparent md:w-auto md:block">
-
-                    <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent"
-                     href="https://help.activatelearning.com/s/">Home</a>
-
-                    <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent" href="https://help.activatelearning.com/s/system-status">System Status</a>
-
-                    <a class="text-gray-700 md:text-white px-5 py-3 border-b md:border-none block md:inline-block hover:text-orange-500 hover:bg-blue-50 md:hover:text-white md:hover:opacity-90 md:hover:bg-transparent"
-                     href="https://help.activatelearning.com/s/contactsupport">Contact Support</a>
-                </nav>
-                <div class="text-white md:ml-16">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
+                    </div>
                 </div>
             </div>
         </header>
