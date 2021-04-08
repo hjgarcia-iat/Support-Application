24397d0968a08a82b90814a6bb7e148d67958b94
Henry Garcia <hgarcia@activatelearning.com>
2021-04-08 14:51:44 -0400

    made search glass smaller

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="page-header mb-10 relative z-20">
     <div class="overlay h-full">
-        <div class="h-full py-2 px-3 flex justify-between items-center md:py-10 md:px-5 md:w-4/6 mx-auto relative">
+        <div class="h-full py-2 px-3 flex justify-between items-center md:py-10 md:px-5 container mx-auto relative">
             <div class="md:hidden text-white" x-show="search===false">
                 <a href="" @click.prevent="mobile_nav = true" x-show="mobile_nav === false">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -59,7 +59,7 @@
 
             <div class="text-white sm:ml-2 lg:ml-4" x-show="search === false">
                 <a @click.prevent="search = true" class="cursor-pointer">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
