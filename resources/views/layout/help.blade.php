702241cc15d4e38e3f8e3ff69066e3667aaabeb1
Henry Garcia <hgarcia@activatelearning.com>
2021-04-09 06:52:44 -0400

    added domain same origin on production environment

--- resources/views/layout/help.blade.php
+++ resources/views/layout/help.blade.php
@@ -4,23 +4,29 @@
 <head>
     @include('layout.partials.meta')
     @include('layout.partials.styles')
+    @if(config('app.env') === 'production')
+        <script>
+            document.domain = 'activatelearning.com';
+        </script>
+    @endif
     <script src="{{ asset('js/alpine.js') }}"></script>
 </head>
 
 <body>
-    <div x-data="{mobile_nav:false, search:false}" class="h-full">
-        <div class="grid-help h-full relative">
-            @include('layout.partials.header')
-            <main role="main" class="page-content">
-                <section class="container mx-auto">
-                    @yield('content')
-                </section>
-            </main>
-            @include('layout.partials.footer')
-        </div>
-        <a @click.prevent="mobile_nav = false" x-show="mobile_nav === true" class="absolute opacity-30 bg-black top-0 left-0 h-full w-full z-10 cursor-pointer"></a>
+<div x-data="{mobile_nav:false, search:false}" class="h-full">
+    <div class="grid-help h-full relative">
+        @include('layout.partials.header')
+        <main role="main" class="page-content">
+            <section class="container mx-auto">
+                @yield('content')
+            </section>
+        </main>
+        @include('layout.partials.footer')
     </div>
-    @yield('scripts')
+    <a @click.prevent="mobile_nav = false" x-show="mobile_nav === true"
+            class="absolute opacity-30 bg-black top-0 left-0 h-full w-full z-10 cursor-pointer"></a>
+</div>
+@yield('scripts')
 </body>
 
 </html>
