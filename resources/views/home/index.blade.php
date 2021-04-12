05783da90b377e1728408a86d1a6c32be739fee0
Henry Garcia <hgarcia@activatelearning.com>
2021-04-12 10:27:16 -0400

    homepage redesign complete

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,67 @@
-<?php
+@extends('layout.help')
+@section('pageTitle','System Status')
+@section('content')
+    <div class="m-4">
+        <h1 class="text-center font-bold text-blue-brand-medium text-3xl mb-2">Welcome to the Activate Learning Support
+            Center</h1>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-12 mx-4 py-4 gap-8">
+        <div class="md:col-span-8">
+            <h2 class="font-bold text-gray-600 mb-4 text-2xl text-center">Browse Our Support Articles</h2>
+            <div class="grid grid-cols-1 md:grid-cols-12 gap-8">
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/curriculum" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">Curriculum & Product Usage</a>
+                </div>
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/csr" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">Customer Support, Sales &amp; Orders</a>
+                </div>
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/product" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">General Product Information</a>
+                </div>
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/tech" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">LMS Integration &amp; Rostering</a>
+                </div>
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/imps" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">New Customer Onboarding</a>
+                </div>
+                <div class="col-span-1 md:col-span-4 h-44 shadow-md rounded-lg">
+                    <a href="https://help.activatelearning.com/s/accounts" class="text-white block h-full flex items-center justify-center bg-lightBlue-800 text-center rounded-lg font-bold text-2xl p-3 hover:bg-lightBlue-700">Teacher &amp; Student Accounts</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="md:col-span-4">
+            <h2 class="font-bold text-gray-600 mb-4 text-2xl text-center">A Bit About Us</h2>
+            <p class="text-gray-600">Activate Learning takes pride in providing high quality support to districts and
+                educators implementing our programs. Our dedicated Support Teams are available Monday through Friday 9
+                AM – 6:30 PM EST.
+
+            </p>
+            
+            <div><img src="{{  asset('img/icon.png') }}" class="h-16 w-16 mx-auto my-4" alt="Activate Learning"></div>
+            
+            <p>
+                This site is designed to provide the resources necessary to address your needs quickly and easily.
+                Please take a moment to explore our support center.
+            </p>
+        </div>
+    </div>
+
+@endsection
+
+@section('scripts')
+    <script>
+        let input = document.getElementById("search-field");
+
+        input.addEventListener("keyup", function (event) {
+
+            if (event.key === 'Enter') {
+                event.preventDefault();
+
+                window.location.href = 'https://help.activatelearning.com/s/global-search/' + input.value
+            }
+        });
+    </script>
+@endsection
