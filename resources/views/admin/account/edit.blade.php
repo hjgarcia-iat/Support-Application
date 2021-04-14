894d00aae00c487776c005fc0a35ebef6b804a10
Henry Garcia <hgarcia@activatelearning.com>
2021-04-14 11:44:19 -0400

    fixed account form on mobile screens

--- resources/views/admin/account/edit.blade.php
+++ resources/views/admin/account/edit.blade.php
@@ -19,28 +19,28 @@
                     <form method="POST" action="{{ route('admin.account.update') }}">
                         @csrf
 
-                        <div class="mt-4 grid grid-cols-6 gap-8 items-center">
-                            <x-label for="name" class="col-span-1" :value="__('Name')"/>
+                        <div class="mt-4 md:grid grid-cols-6 gap-8 items-center">
+                            <x-label for="name" class="col-span-1 font-bold uppercase tracking-wide" :value="__('Name')"/>
                             <x-input id="name" class="block mt-1 w-full col-span-5" type="name" name="name" :value="old('name', auth()->user()->name)"
                                      autofocus/>
                         </div>
 
-                        <div class="mt-4 grid grid-cols-6 gap-8 items-center">
-                            <x-label for="email" class="col-span-1" :value="__('Email')"/>
+                        <div class="mt-4 md:grid grid-cols-6 gap-8 items-center">
+                            <x-label for="email" class="col-span-1 font-bold uppercase tracking-wide" :value="__('Email')"/>
                             <x-input id="email" class="block mt-1 w-full col-span-5" type="email" name="email"
                                      :value="old('email', auth()->user()->email)" autofocus/>
                         </div>
 
-                        <div class="mt-4 grid grid-cols-6 gap-8 items-center">
-                            <x-label for="password" class="col-span-1" :value="__('Password')"/>
+                        <div class="mt-4 md:grid grid-cols-6 gap-8 items-center">
+                            <x-label for="password" class="col-span-1 font-bold uppercase tracking-wide" :value="__('Password')"/>
                             <x-input id="password" class="block mt-1 w-full col-span-5"
                                      type="password"
                                      name="password"
                                      autocomplete="current-password"/>
                         </div>
 
-                        <div class="mt-4 grid grid-cols-6 gap-8 items-center">
-                            <x-label for="password_confirmation" class="col-span-1" :value="__('Confirm Password')"/>
+                        <div class="mt-4 md:grid grid-cols-6 gap-8 items-center">
+                            <x-label for="password_confirmation" class="col-span-1 font-bold uppercase tracking-wide" :value="__('Confirm Password')"/>
                             <x-input id="password_confirmation" class="block mt-1 w-full col-span-5"
                                      type="password"
                                      name="password_confirmation"
