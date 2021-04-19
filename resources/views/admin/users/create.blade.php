5b5416a389691ffdce1962930e5f07b030157b33
Henry Garcia <hgarcia@activatelearning.com>
2021-04-19 06:55:29 -0400

    deploy hq

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -12,6 +12,7 @@
         </div>
     </x-slot>
 
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
