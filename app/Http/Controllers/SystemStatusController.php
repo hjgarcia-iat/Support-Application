cd5945c25bea8bc0c07cc030b705f1771e29c7b2
Henry Garcia <hgarcia@activatelearning.com>
2021-04-30 10:07:22 -0400

    removed carbon reference

--- app/Http/Controllers/SystemStatusController.php
+++ app/Http/Controllers/SystemStatusController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Status;
-use Carbon\Carbon;
 
 class SystemStatusController extends Controller
 {
     public function index()
     {
         $statuses = Status::latest()
-            ->where('created_at', '>=', Carbon::now()->subDays(7))
+            ->where('created_at', '>=', now()->subDays(7))
             ->get();
 
         return view('system_status.index', \compact('statuses'));
