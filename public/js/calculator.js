fc70a35ea4519e50c78ca5279d3507653a21e116
Henry Garcia <hgarcia@activatelearning.com>
2021-06-02 14:20:41 -0400

    removed astrobiology

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -3677,7 +3677,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.grid[data-v-1d45d9cc] {\n    display: grid;\n    grid-template-columns: 1fr 1fr 1fr;\n    gap: 0 1em;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.grid[data-v-1d45d9cc] {\r\n    display: grid;\r\n    grid-template-columns: 1fr 1fr 1fr;\r\n    gap: 0 1em;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
