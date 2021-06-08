7a02aa81244014d82e750bffc4391127d1e44183
Henry Garcia <hgarcia@activatelearning.com>
2021-06-08 11:56:35 -0400

    added categories and articles to the homepage

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -12,6 +12,32 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="grid grid-cols-12 gap-12">
+
+                        <div class="col-span-4 text-center text-white shadow-sm">
+                            <a href="{{ route('admin.categories') }}"
+                               class="block w-full h-full p-8 bg-lightBlue-700 hover:bg-lightBlue-800">
+                                <h3 class="text-3xl mb-8">Categories</h3>
+                                <div class="flex justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+                                    </svg>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-span-4 text-center text-white shadow-sm">
+                            <a href="{{ route('admin.articles') }}"
+                               class="block w-full h-full p-8 bg-lightBlue-700 hover:bg-lightBlue-800">
+                                <h3 class="text-3xl mb-8">Articles</h3>
+                                <div class="flex justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+                                    </svg>
+                                </div>
+                            </a>
+
+                        </div>
+
                         <div class="col-span-4 text-center text-white shadow-sm">
                             <a href="{{ route('admin.tickets') }}"
                                class="block w-full h-full p-8 bg-lightBlue-700 hover:bg-lightBlue-800">
