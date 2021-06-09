533afb68fcb37dffeed3a9b5a1ed1602c130978d
Henry Garcia <hgarcia@activatelearning.com>
2021-06-09 19:02:42 -0400

    added categories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Status;
-use App\Ticket;
+use App\Category;
 use App\User;
-use Carbon\Carbon;
-use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +15,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::factory()->create([
+            'name' => 'Curriculum & Product Usage',
+            'slug' => 'Curriculum and Product Usage'
+        ]);
+
+        Category::factory()->create([
+            'name' => 'Customer Support, Sales & Orders',
+            'slug' => 'Customer Support, Sales and Orders'
+        ]);
+
+        Category::factory()->create([
+            'name' => 'General Product Information',
+            'slug' => 'General Product Information'
+        ]);
+
+        Category::factory()->create([
+            'name' => 'LMS Integration & Rostering',
+            'slug' => 'LMS Integration and Rostering'
+        ]);
+
+        Category::factory()->create([
+            'name' => 'New Customer Onboarding',
+            'slug' => 'New Customer Onboarding'
+        ]);
+
+        Category::factory()->create([
+            'name' => 'Teacher & Student Accounts',
+            'slug' => 'Teacher and Student Accounts'
+        ]);
+
         User::factory()->create(['email' => 'test@email.com']);
     }
 }
