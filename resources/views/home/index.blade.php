ee1e0fb7c0489c61a10b28a03afb02c4c44e972c
Henry Garcia <hgarcia@activatelearning.com>
2021-06-29 09:22:49 -0400

    added chat to home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -38,22 +38,22 @@
                 This site is designed to provide the resources necessary to address your needs quickly and easily.
                 Please take a moment to explore our support center.
             </p>
+
+
+            <a id="liveagent_button_online_5733c000000CrAT" href="javascript://Chat" style="display: none;" onclick="liveagent.startChat('5733c000000CrAT')"><!-- Online Chat Content --></a><div id="liveagent_button_offline_5733c000000CrAT" style="display: none;"><!-- Offline Chat Content --></div><script type="text/javascript">
+                if (!window._laq) { window._laq = []; }
+                window._laq.push(function(){liveagent.showWhenOnline('5733c000000CrAT', document.getElementById('liveagent_button_online_5733c000000CrAT'));
+                    liveagent.showWhenOffline('5733c000000CrAT', document.getElementById('liveagent_button_offline_5733c000000CrAT'));
+                });</script>
+
         </div>
     </div>
 
 @endsection
 
 @section('scripts')
-    <script>
-        let input = document.getElementById("search-field");
-
-        input.addEventListener("keyup", function (event) {
-
-            if (event.key === 'Enter') {
-                event.preventDefault();
-
-                window.location.href = 'https://help.activatelearning.com/s/global-search/' + input.value
-            }
-        });
+    <script type='text/javascript' src='https://c.la3-c2-ph2.salesforceliveagent.com/content/g/js/52.0/deployment.js'></script>
+    <script type='text/javascript'>
+        liveagent.init('https://d.la3-c2-ph2.salesforceliveagent.com/chat', '5723c000000CqiR', '00D30000001GDNY');
     </script>
 @endsection
