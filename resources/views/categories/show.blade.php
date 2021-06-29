57d8045a7e1d23ad735053f1613ec5a0d40c71f2
Henry Garcia <hgarcia@activatelearning.com>
2021-06-29 09:28:13 -0400

    added space for to breadcrumbs

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.categories')
 @section('pageTitle',$category->name)
 @section('content')
+    <div class="mx-4">
+        {{ Breadcrumbs::render('articles', $category) }}
+    </div>
 
-    {{ Breadcrumbs::render('articles', $category) }}
 
     <div class="m-4">
         <h1 class="font-bold text-blue-brand-medium text-3xl mb-2">{{ $category->name }}</h1>
@@ -11,19 +13,22 @@
     <div class="grid grid-cols-1 md:grid-cols-12 mx-4 py-4 gap-x-12">
 
         @if($category->children->isNotEmpty())
-        <div class="md:col-span-3">
-            <ul>
-                @foreach($category->children as $child)
-                    <li><a href="{{ route('categories.show', $child->slug) }}" class="w-full bg-gray-100 rounded mb-2 py-2 px-2 block hover:bg-gray-200">{{ $child->name }}</a></li>
-                @endforeach
-            </ul>
-        </div>
+            <div class="md:col-span-3">
+                <ul>
+                    @foreach($category->children as $child)
+                        <li><a href="{{ route('categories.show', $child->slug) }}"
+                               class="w-full bg-gray-100 rounded mb-2 py-2 px-2 block hover:bg-gray-200">{{ $child->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         @endif
 
         <div class="md:col-span-9">
             @foreach($category->articles as $article)
                 <h2 class="text-2xl mb-6">
-                    <a href="{{ route('categories.articles.show',[$category->slug, $article->slug]) }}" class="hover:text-orange-600 hover:underline">{{ $article->name }}</a>
+                    <a href="{{ route('categories.articles.show',[$category->slug, $article->slug]) }}"
+                       class="hover:text-orange-600 hover:underline">{{ $article->name }}</a>
                 </h2>
             @endforeach
         </div>
