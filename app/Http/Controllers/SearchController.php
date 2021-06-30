7bc04f82e141399259394e53527b0b27909539ea
Henry Garcia <hgarcia@activatelearning.com>
2021-06-30 06:34:47 -0400

    switched search to be about articles and not categories

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use App\Category;
 
 class SearchController extends Controller
 {
     public function index()
     {
-        //when we search we only want subcategories not root
-        $categories = Category::where('parent_id', '<>', null)
-            ->where('name','LIKE','%'. request('q') .'%')
+        //it needs to look through articles
+        $articles = Article::where('name','LIKE','%'. request('q') .'%')
             ->paginate(20);
 
-        return view('search.index', ['categories' => $categories]);
+        return view('search.index', ['articles' => $articles]);
     }
 }
--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -5,20 +5,20 @@
     <div class="m-4 flex justify-between">
         <h1 class="font-bold text-blue-brand-medium text-3xl mb-2">Search Results</h1>
         <div>
-            Number of Results: <strong>{{ $categories->total() }}</strong>
+            Number of Results: <strong>{{ $articles->total() }}</strong>
         </div>
     </div>
 
     <div class="grid grid-cols-1 gap-y-4 mx-4">
-        @foreach($categories as $category)
+        @foreach($articles as $article)
             <h2 class="text-2xl">
-                <a class="hover:text-orange-500 hover:underline" href="{{ route('categories.show', $category->slug) }}">{{ $category->name }}</a>
+                <a class="hover:text-orange-500 hover:underline" href="{{ route('categories.show', $article->categories->first()->slug) }}">{{ $article->name }}</a>
             </h2>
         @endforeach
     </div>
 
     <div class="m-4">
-        {{ $categories->withQueryString()->links() }}
+        {{ $articles->withQueryString()->links() }}
     </div>
 @endsection
 
