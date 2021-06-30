a60645a01be01dfcd3c8b6a062e220ba50e2ad5d
Henry Garcia <hgarcia@activatelearning.com>
2021-06-30 07:21:47 -0400

    changes to search area

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.categories')
-@section('pageTitle','Search')
+@extends('layouts.categories')@section('pageTitle','Search')
 @section('content')
 
     <div class="m-4 flex justify-between">
@@ -12,8 +11,15 @@
     <div class="grid grid-cols-1 gap-y-4 mx-4">
         @foreach($articles as $article)
             <h2 class="text-2xl">
-                <a class="hover:text-orange-500 hover:underline" href="{{ route('categories.show', $article->categories->first()->slug) }}">{{ $article->name }}</a>
+                <a class="hover:text-orange-500 hover:underline"
+                    href="{{ route('categories.show', $article->categories->first()->slug) }}">{{ $article->name }}</a>
             </h2>
+
+            {!!  \Illuminate\Support\Str::limit($article->content,$words = 200, $end='...') !!}
+
+            <p>
+                <a href="{{ route('categories.show', $article->categories->first()->slug) }}" class="hover:underline">View Article</a>
+            </p>
         @endforeach
     </div>
 
